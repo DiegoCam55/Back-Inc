773c5b1ca50618249477d66d06ce5c366060fe05
DiegoCam55 <cameldiego55@gmail.com>
2025-06-12 17:25:22 -0500

    Refactorice los estilos de los componentes de la tarjeta y mejore el manejo de errores de imagen

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -8,11 +8,12 @@ import {
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Producto } from '../../dashboard/interfaces/ProductoCategoria';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
 })
@@ -23,5 +24,12 @@ export class CardComponent {
   // eminir el evento para agregar un producto al carrito
   addToCart(arg0: Producto) {
     this.addToCartEvent.emit(arg0);
+  }
+
+  onImageError(event: Event) {
+    const img = event.target as HTMLImageElement;
+    console.log('Error al cargar la imagen:', img.src);
+    img.src =
+      'https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg'; // Imagen por defecto
   }
 }
